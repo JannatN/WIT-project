79d8840f5ec8a096c0c0ac833f555be56f534718
JannatN <jannatfaisalnatsheh@gmail.com>
2020-08-27 16:39:07 +0300

    multistep form and questions

--- Public/firstPage.js
+++ Public/firstPage.js
@@ -1,75 +1,56 @@
 console.log("Reloaded");
 
 // dom variables
-var msf_getFsTag = document.getElementsByTagName("fieldset");
+var getFieldElements = document.getElementsByTagName("fieldset");
 
 // declaring the active fieldset & the total fieldset count
-var msf_form_nr = 0;
-var fieldset = msf_getFsTag[msf_form_nr];
-fieldset.className = "msf_show";
-
-// creates and stores a number of bullets
-var msf_bullet_nr = "<div class='msf_bullet'></div>";
-var msf_length = msf_getFsTag.length;
-for (var i = 1; i < msf_length; ++i) {
-    msf_bullet_nr += "<div class='msf_bullet'></div>";
-};
-// injects bullets
-var msf_bullet_o = document.getElementsByClassName("msf_bullet_o");
-for (var i = 0; i < msf_bullet_o.length; ++i) {
-    var msf_b_item = msf_bullet_o[i];
-    msf_b_item.innerHTML = msf_bullet_nr;
-};
-
-// removes the first back button & the last next button
-document.getElementsByName("back")[0].className = "msf_hide";
-document.getElementsByName("next")[msf_bullet_o.length - 1].className = "hide";
-
-// Makes the first dot active
-var msf_bullets = document.getElementsByClassName("msf_bullet");
-msf_bullets[msf_form_nr].className += " msf_bullet_active";
+var count = 0;
+var fieldset = getFieldElements[count];
 
 // Validation loop & goes to the next step
-function msf_btn_next() {
-    var msf_val = true;
+function btn_next() {
+	var isClicked = true;
 
-    var msf_fs = document.querySelectorAll("fieldset")[msf_form_nr];
-    var msf_fs_i_count = msf_fs.querySelectorAll("input").length;
+	var fieldsetAction = document.querySelectorAll("fieldset")[count];
+	var fieldCount = fieldsetAction.querySelectorAll("input").length;
 
-    for (i = 0; i < msf_fs_i_count; ++i) {
-        var msf_input_s = msf_fs.querySelectorAll("input")[i];
-        if (msf_input_s.getAttribute("type") === "button") {
-            // nothing happens
-        } else {
-            if (msf_input_s.value === "") {
-                msf_input_s.style.backgroundColor = "pink";
-                msf_val = false;
-            } else {
-                if (msf_val === false) {} else {
-                    msf_val = true;
-                    msf_input_s.style.backgroundColor = "lime";
-                }
-            }
-        };
-    };
-    if (msf_val === true) {
-        // goes to the next step
-        var selection = msf_getFsTag[msf_form_nr];
-        selection.className = "hide";
-        msf_form_nr = msf_form_nr + 1;
-        var selection = msf_getFsTag[msf_form_nr];
-        selection.className = "msf_show";
-        // refreshes the bullet
-        var msf_bullets_a = msf_form_nr * msf_length + msf_form_nr;
-        msf_bullets[msf_bullets_a].className += " msf_bullet_active";
-    }
+	for (i = 0; i < fieldCount; ++i) {
+		var fieldInput = fieldsetAction.querySelectorAll("input")[i];
+		if (fieldInput.getAttribute("type") === "button") {
+			// nothing happens
+		} else {
+			if (fieldInput.value === "") {
+				fieldInput.style.backgroundColor = "pink";
+				isClicked = false;
+			} else {
+				if (isClicked === false) { } else {
+					isClicked = true;
+					fieldInput.style.backgroundColor = "none";
+				}
+			}
+		};
+	};
+	if (isClicked === true) {
+		// goes to the next step
+		var selection = getFieldElements[count];
+		selection.className = "hide";
+		count = count + 1;
+		var selection = getFieldElements[count];
+		selection.className = "Card";
+
+	}
 };
 
 // goes one step back
-function msf_btn_back() {
-    msf_getFsTag[msf_form_nr].className = "hide";
-    msf_form_nr = msf_form_nr - 1;
-    msf_getFsTag[msf_form_nr].className = "msf_showhide";
+function btn_backToForm1() {
+	getFieldElements[count].className = "hide";
+	count = count - 1;
+	getFieldElements[count].className = "show";
+};
+function btn_backToForm2() {
+	getFieldElements[count].className = "hide";
+	count = count - 1;
+	getFieldElements[count].className = "Card";
 };
 
 console.log("loaded");
