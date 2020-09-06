98b7d874ad41aca08efb9d5691edc38d1eb83914
JannatN <jannatfaisalnatsheh@gmail.com>
2020-09-06 14:15:52 +0300

    edit desgin

--- JS/secondPage.js
+++ JS/secondPage.js
@@ -8,18 +8,19 @@ document.getElementById("contact").innerHTML = sessionStorage.getItem('contact')
 
 
 
-$("#profileImage").click(function(e) {
+
+$("#profileImage").click(function (e) {
     $("#imageUpload").click();
 });
 
-function fasterPreview( uploader ) {
-    if ( uploader.files && uploader.files[0] ){
-          $('#profileImage').attr('src', 
-             window.URL.createObjectURL(uploader.files[0]) );
+function fasterPreview(uploader) {
+    if (uploader.files && uploader.files[0]) {
+        $('#profileImage').attr('src',
+            window.URL.createObjectURL(uploader.files[0]));
     }
 }
 
-$("#imageUpload").change(function(){
-    fasterPreview( this );
+$("#imageUpload").change(function () {
+    fasterPreview(this);
 });
 
